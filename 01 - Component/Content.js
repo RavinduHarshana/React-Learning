3b7fd929ceaa8a584e628c0bc059f32ffc727a72
RavinduHarshana <ravinduharshana2001@gmail.com>
2024-08-22 12:32:17 +0530

    React Component

---
+++ 01 - Component/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Content.css";
+
+export default function Content() {
+  return (
+    <div>
+      <div className="con">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+          aliquam, officiis architecto adipisci facilis repellat tempora enim
+          tenetur reiciendis! Nihil harum quas animi ratione eum iusto
+          cupiditate assumenda non quos.
+        </p>
+      </div>
+    </div>
+  );
+}
