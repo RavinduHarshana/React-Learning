862c66072fba413fd68524dc57665fcacf9fdafd
RavinduHarshana <ravinduharshana2001@gmail.com>
2024-08-31 23:43:34 +0530

    Simple Array And Dynamic List

---
+++ 07 - ListRendering/Listitemarray.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Listitemarray() {
+    const Fruite=["Apple","Banana","Mango","Greops"];
+
+    const listitem=Fruite.map(fuite=><li>{fuitee}</li>)
+
+  return (
+    <div>Listitemarray</div>
+  )
+}
